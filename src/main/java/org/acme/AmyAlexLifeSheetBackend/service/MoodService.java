687e8d79733e739dd797770b11f3a0202248a806
Amy <yambeddm@gmail.com>
2024-04-23 17:10:17 +0200

    added mood infos

---
+++ src/main/java/org/acme/AmyAlexLifeSheetBackend/service/MoodService.java
@@ -0,0 +1,15 @@
+package org.acme.AmyAlexLifeSheetBackend.service;
+
+import org.acme.AmyAlexLifeSheetBackend.dto.MoodDto;
+
+import java.util.List;
+
+public interface MoodService {
+    MoodDto createMood(MoodDto moodDto);
+    MoodDto getMoodById(Long moodId);
+    List<MoodDto>  getMoodAll();
+
+    MoodDto updateMood(long moodId, MoodDto updateMood);
+
+    void deleteMood(Long moodId);
+}
