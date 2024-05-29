c122c0aa897e9816235bdc0067d85881e49d0805
Amy <yambeddm@gmail.com>
2024-05-29 14:15:24 +0200

    initial commit

--- src/main/java/org/acme/AmyAlexLifeSheetBackend/controller/MoodController.java
+++ src/main/java/org/acme/AmyAlexLifeSheetBackend/controller/MoodController.java
@@ -6,12 +6,12 @@ import org.acme.AmyAlexLifeSheetBackend.repository.MoodRepository;
 import org.acme.AmyAlexLifeSheetBackend.service.MoodService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-//test
-@RestController
-@RequestMapping("/mood")
+
+@RestController//returns a domain object instead of a view
+@RequestMapping("/mood") //maps HTTP requests
 public class MoodController extends BaseController<Mood, Long> {
+//entity's type / entity's identifier
     private final MoodService moodService;
     public MoodController(MoodRepository repository, MoodService moodService) {
         super(repository);
@@ -19,12 +19,15 @@ public class MoodController extends BaseController<Mood, Long> {
     }
     @GetMapping("/tempdata")
     public ResponseEntity<List<MoodDto>> getMood(
+            //parameters are optional
             @RequestParam(required = false) Integer year,
             @RequestParam(required = false) Integer month,
             @RequestParam(required = false) Integer week,
             @RequestParam(required = false) Integer day
     ) {
+        //calls getMoods from Service with provided params
         List<MoodDto> moods = moodService.getMoodsByParams(year, month, week, day);
+        //response body will be a list of HobbiesDto objects
         return ResponseEntity.ok(moods);
     }
 
--- src/main/java/org/acme/AmyAlexLifeSheetBackend/service/impl/MoodServiceImpl.java
+++ src/main/java/org/acme/AmyAlexLifeSheetBackend/service/impl/MoodServiceImpl.java
@@ -19,21 +19,21 @@ public class MoodServiceImpl implements MoodService {
     @Override
     public List<MoodDto> getMoodsByParams(Integer year, Integer month, Integer week, Integer day) {
         List<Mood> moods;
-        //Construire la requête en fonction des paramètres fournis
+        //request built around provided params
         if (year != null && month != null && week != null && day != null) {
-            // Si toutes les valeurs sont fournies, recherche par jour
+            // all values provided, filter by day
             moods = moodRepository.findByYearAndMonthAndWeekAndDay(year, month, week, day);
         } else if (year != null && month != null && week != null) {
-            // Si year, month et week sont fournis, recherche par semaine
+            // filter by week
             moods = moodRepository.findByYearAndMonthAndWeek(year, month, week);
         } else if (year != null && month != null) {
-            // Si year et month sont fournis, recherche par mois
+            // filter by month
             moods = moodRepository.findByYearAndMonth(year, month);
         } else if (year != null) {
-            // Si year est fourni, recherche par année
+            // filter by year
             moods = moodRepository.findByYear(year);
         } else {
-            // Si aucun paramètre n'est fourni, retourner toutes les données (non filtrées)
+            // no params = return all values
             moods = moodRepository.findAll();
         }
         return moods.stream()
